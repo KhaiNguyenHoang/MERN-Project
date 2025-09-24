5fa99a7a65a24b8bb070026845be7492d1d09a79
KhaiNguyenHoang <khaiproject1234@gmail.com>
2025-09-24 22:12:21 +0700

    Update

--- ProductStore/BackEnd/server.js
+++ ProductStore/BackEnd/server.js
@@ -1,3 +1,4 @@
+// TODO: Import the necessary modules
 import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
@@ -6,6 +7,7 @@ import router from "./routes/product.route.js";
 dotenv.config();
 const app = express();
 const port = process.env.PORT || 5000;
+
 app.use(express.json()); // TODO: Accept us to use JSON data in the body of the request
 app.use("/", router);
 app.listen(port, () => {
