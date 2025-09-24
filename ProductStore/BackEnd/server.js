cafe66d0829e55d5cdc136ee070f30c4406985ef
KhaiNguyenHoang <khaiproject1234@gmail.com>
2025-09-24 16:27:57 +0700

    Update port to .env

--- ProductStore/BackEnd/server.js
+++ ProductStore/BackEnd/server.js
@@ -5,10 +5,11 @@ import router from "./routes/product.route.js";
 
 dotenv.config();
 const app = express();
+const port = process.env.PORT || 5000;
 app.use(express.json()); // TODO: Accept us to use JSON data in the body of the request
 app.use("/", router);
-app.listen(5000, () => {
+app.listen(port, () => {
   console.log(process.env.MONGO_URI);
   connectDB();
-  console.log("Server started at http://localhost:5000");
+  console.log(`Server started at http://localhost:${port}`);
 });
