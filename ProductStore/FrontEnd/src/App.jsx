1dbec3c02b0ce81ccbcaa5139f8f879c9081ea5d
KhaiNguyenHoang <khaiproject1234@gmail.com>
2025-09-25 08:40:00 +0700

    Update

--- ProductStore/FrontEnd/src/App.jsx
+++ ProductStore/FrontEnd/src/App.jsx
@@ -1,13 +1,7 @@
-import reactLogo from "./assets/react.svg";
-import { Button, Center, HStack } from "@chakra-ui/react";
-import viteLogo from "/vite.svg";
+import { useState } from "react";
 
 function App() {
-  return (
-    <Center flexDir="column" gap="8" minH="dvh">
-      <Button>Click me</Button>
-    </Center>
-  );
+  return <></>;
 }
 
 export default App;
