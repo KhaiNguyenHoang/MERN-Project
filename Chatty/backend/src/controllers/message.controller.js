57ac0c4e2cf45acbb2fd81f42f7bc3c9ddfd40c0
KhaiNguyenHoang <khaiproject1234@gmail.com>
2025-09-26 09:41:17 +0700

    Update message

---
+++ Chatty/backend/src/controllers/message.controller.js
@@ -0,0 +1,65 @@
+import Message from "../models/message.model.js";
+import User from "../models/user.model.js";
+
+export const getUserForSidebar = async (req, res) => {
+  try {
+    const users = await User.find({ _id: { $ne: req.user.id } });
+    return res.status(200).json({ code: 200, message: "Users", users });
+  } catch (err) {
+    console.error(err);
+    return res.status(500).json({ code: 500, message: "Server error" });
+  }
+};
+
+export const getMessages = async (req, res) => {
+  try {
+    const { id: userToChatId } = req.params;
+    const myId = req.user._id;
+
+    const messages = await Message.find({
+      $or: [
+        { senderId: myId, receiverId: userToChatId },
+        { senderId: userToChatId, receiverId: myId },
+      ],
+    });
+
+    res.status(200).json(messages);
+  } catch (error) {
+    console.log("Error in getMessages controller: ", error.message);
+    res.status(500).json({ error: "Internal server error" });
+  }
+};
+
+export const sendMessage = async (req, res) => {
+  try {
+    const { text, image } = req.body;
+    const { id: receiverId } = req.params;
+    const senderId = req.user._id;
+
+    let imageUrl;
+    if (image) {
+      // Upload base64 image to cloudinary
+      const uploadResponse = await cloudinary.uploader.upload(image);
+      imageUrl = uploadResponse.secure_url;
+    }
+
+    const newMessage = new Message({
+      senderId,
+      receiverId,
+      text,
+      image: imageUrl,
+    });
+
+    await newMessage.save();
+
+    const receiverSocketId = getReceiverSocketId(receiverId);
+    if (receiverSocketId) {
+      io.to(receiverSocketId).emit("newMessage", newMessage);
+    }
+
+    res.status(201).json(newMessage);
+  } catch (error) {
+    console.log("Error in sendMessage controller: ", error.message);
+    res.status(500).json({ error: "Internal server error" });
+  }
+};
